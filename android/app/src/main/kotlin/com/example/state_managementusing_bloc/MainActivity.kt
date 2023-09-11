a44852c0538414d192b7876ab7b8b142d99e9940
Tanisha Arora <Tanisha.Arora@SRMTECHSOL.Local>
2023-09-11 15:42:15 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/state_managementusing_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.state_managementusing_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,24 @@
+class GroceryData{
+  static List<Map<String, dynamic>> groceryList = [
+    {
+      'name': 'Apples',
+      'quantity': 5,
+      'price': 2.99,
+    },
+    {
+      'name': 'Bananas',
+      'quantity': 8,
+      'price': 1.99,
+    },
+    {
+      'name': 'Milk',
+      'quantity': 2,
+      'price': 3.49,
+    },
+    {
+      'name': 'Bread',
+      'quantity': 1,
+      'price': 2.29,
+    },
+  ];
+}
